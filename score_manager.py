7467384fb8aab6902eb81d6e7118009b6691decd
Katya <pvlnkkate@gmail.com>
2026-03-12 18:57:30 +0200

    Add ScoreManager add method for updating score

--- score_manager.py
+++ score_manager.py
@@ -5,3 +5,8 @@ class ScoreManager:
         self.score = 0
         self.high_score = 0
         self.font = pygame.font.Font(None, 36)
+
+    def add(self, points: int) -> None:
+        self.score += points
+        if self.score > self.high_score:
+            self.high_score = self.score
