c95bc46d411a2ddf76c7af434be7a47ee2e54d9d
Artem <artem4sav@gmail.com>
2026-03-17 22:10:24 +0200

    update init

--- ball.py
+++ ball.py
@@ -8,12 +8,16 @@ class Ball:
         self.start_x = x  # запам'ятали старт
         self.start_y = y
         self.radius = radius # зберегти радіус
-        self.dx = dx # зберегти швидкість руху
-        self.dy = dy # зберегти швидкість руху
-        # TODO: додати start_dx і start_dy і зберегти у них початкові значення dx і dy
-        # TODO: додати speed_multiplier = 1.0
-        # TODO: додати max_multiplier = 2.5
-        # TODO: додати acceleration_rate = 0.001
+        # Зберігаємо початкову швидкість, щоб метод reset() міг її відновити
+        self.dx = dx
+        self.dy = dy
+        self.start_dx = dx
+        self.start_dy = dy
+
+        # Параметри прискорення
+        self.speed_multiplier = 1.0  # Поточний множник швидкості
+        self.max_multiplier = 2.5  # Максимальна межа (щоб м'яч не став занадто швидким)
+        self.acceleration_rate = 0.001  # На скільки збільшуємо швидкість кожного кадру
 
     # метод руху м’яча, що він робить: змінює координати кожен кадр, використовує dx і dy
     def move(self) -> None:
