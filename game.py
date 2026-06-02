f2c374d1b752d668a526652dd39e829cff91c8e2
Karina Lukiianova <karinalukiyanova2007@knu.ua>
2026-06-02 19:23:55 +0300

    Update game.py

--- game.py
+++ game.py
@@ -167,7 +167,7 @@ class Game:
         if ball_rect.colliderect(platform_rect):
             if self.ball.dy > 0:
                 self.ball.bounce_y()  # bounce: якщо зіткнення сталося, потрібно змінити напрям руху м’яча.
-                self.ball.y = self.platform.y - self.ball.radius  # після зіткнення скоригувати позицію м'яча, щоб не було повторного зіткнення
+                self.ball.y = self.platform.y - self.ball.radius  # скоригувати позицію, щоб не було повторного зіткнення
 
     def draw_ui(self) -> None:
         # score
