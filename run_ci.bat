a2e7c62d4cfeb02cde5caf93b70a06ebacd44374
Polina <polinaonipcuk@gmail.com>
2026-06-02 16:41:06 +0300

    fix local CI pipeline script

--- run_ci.bat
+++ run_ci.bat
@@ -1,9 +1,9 @@
 @echo off
 echo === Запуск тестів ===
-pytest tests/ --html=report.html --self-contained-html
+C:\Users\risen\AppData\Local\Programs\Python\Python313\Scripts\pytest tests/ --html=report.html --self-contained-html
 
 echo === Перевірка стилю коду ===
-flake8 . --max-line-length=120
+C:\Users\risen\AppData\Local\Programs\Python\Python313\Scripts\flake8 . --max-line-length=120
 
 echo === Готово ===
 pause
